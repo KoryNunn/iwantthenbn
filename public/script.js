4de426d5ddc27f88c2009b88a8aca90f1f3c02ce
Kory Nunn <knunn187@gmail.com>
2013-09-11 21:00:54 +1000

    Slightly less disgusting js

--- public/script.js
+++ public/script.js
@@ -1,148 +1,193 @@
-function createXHR()
-{
-    var xhr;
-    if (window.ActiveXObject)
+;(function(){
+    function createXHR()
     {
-        try
+        var xhr;
+        if (window.ActiveXObject)
         {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            try
+            {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch(e)
+            {
+                alert(e.message);
+                xhr = null;
+            }
         }
-        catch(e)
+        else
         {
-            alert(e.message);
-            xhr = null;
+            xhr = new XMLHttpRequest();
+        }
+
+        return xhr;
+    }
+
+    function round(number, places){
+        var placesTimesTen = places * 10;
+        return parseInt(number * placesTimesTen) / placesTimesTen;
+    }
+
+    var appData = {
+            chartTime: 0,
+            rate:0,
+            displayRate: 0,
+            displayCount: 0,
+            signatureCount: 0
+        },
+        rateChart,
+        dataInterval = 3000,
+        displayInterval = 20,
+        dataDisplayRatio = dataInterval / displayInterval,
+        signatureCountElement,
+        signatureRateElement,
+        results = crel('div', {'class':'results'},
+            crel('div', {'class':'countWrapper'},
+                crel('span','Number of signatures:'),
+                signatureCountElement = crel('span',{'class':'count'}, 'Loading..')
+            ),
+            crel('div', {'class':'rateWrapper'},
+                crel('span','Signatures per second:'),
+                signatureRateElement = crel('span',{'class':'rate'}, 'Loading..')
+            )
+        );
+
+    function updateRateElement(rate){
+        signatureRateElement.textContent = rate ? round(rate, 1) : 'loading..';
+        signatureRateElement.style['font-size'] = Math.min(((rate + 1) * 50), 300) + 'px';
+        var color = 'hsl(' + (50-(rate * 4)) + ',' + (rate * 100) + '%,' + rate * 100 + '%)';
+        var shadowColor = 'hsl(' + (50-(rate * 4)) + ',' + (rate * 100) + '%,50%)';
+        var shadow = '0 0 ' + rate * 100 + 'px ' + shadowColor;
+        signatureRateElement.style['color'] = color;
+        signatureRateElement.style['text-shadow'] = shadow + ', ' + shadow;
+    }
+    
+    function updateRateGraph(){
+        if(rateChart){
+            var series = rateChart.series[0],
+                shift = series.data.length > 20;
+
+            rateChart.series[0].addPoint([appData.chartTime+=10, round(appData.displayRate, 2)], true, shift);
         }
     }
-    else
-    {
-        xhr = new XMLHttpRequest();
+
+    function updateRate(){
+        if(!appData.displayRate){
+            appData.displayRate = appData.rate;
+        }
+        appData.displayRate += (appData.rate - appData.displayRate) / dataDisplayRatio;
+        console.log(appData.displayRate);
+        updateRateElement(appData.displayRate);
     }
 
-    return xhr;
-}
+    function setRate(rate){
+        appData.rate = rate;
+    }
 
-var result,
-    rateChart,
-    popChart,
-    chartTime = 0,
-    dataInterval = 3000,
-    displayInterval = 100,
-    dataDisplayRatio = dataInterval / displayInterval,
-    displayCount = 0,
-    signatureCount,
-    signatureRate,
-    results = crel('div', {'class':'results'},
-        crel('div', {'class':'countWrapper'},
-            crel('span','Number of signatures:'),
-            signatureCount = crel('span',{'class':'count'}, 'Loading..')
-        ),
-        crel('div', {'class':'rateWrapper'},
-            crel('span','Signatures per second:'),
-            signatureRate = crel('span',{'class':'rate'}, 'Loading..')
-        )
-    );
+    function handleData(data){
+        var result = JSON.parse(data);
 
-function getData(){
-    var xhr = createXHR();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === 4)
+        appData.rate = result.rate;
+        appData.signatureCount = result.signatureCount;
+        setRate(appData.rate);
+    }
+
+    function getData(){
+        var xhr = createXHR();
+        xhr.onreadystatechange = function()
         {
-            result = JSON.parse(xhr.responseText);
-
-            if (!displayCount) {
-                // Set the initial value
-                displayCount = result.signatureCount ? result.signatureCount : 0;
+            if (xhr.readyState === 4)
+            {
+                handleData(xhr.responseText);
             }
-            signatureRate.textContent = result.rate ? parseInt(result.rate * 10) / 10 : 'loading..';
+        };
+        xhr.open('GET', '/signatures?_=' + new Date().getTime(), true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send();
+    }
 
-            if(rateChart){
-                var series = rateChart.series[0],
-                    shift = series.data.length > 20; // shift if the series is longer than 20
-
-                // add the point
-                rateChart.series[0].addPoint([chartTime+=10, result.rate], true, shift);
-            }
-
-        }
-    };
-    xhr.open('GET', '/signatures?_=' + new Date().getTime(), true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send();
-}
-
-window.onload = function(){
-
-    document.getElementsByClassName('current')[0].appendChild(results);
-    getData();
-
-    setInterval(getData, dataInterval);
-
-    setInterval(function(){
-        if (!result) {
-            return;
+    function updateTotal(){
+        if (!appData.displayCount) {
+            // Set the initial value
+            appData.displayCount = appData.signatureCount ? appData.signatureCount : 0;
         }
         // play catchup
-        displayCount += (result.signatureCount - displayCount) / dataDisplayRatio;
-        signatureCount.textContent = parseInt(displayCount) || 'loading..';
+        appData.displayCount += (appData.signatureCount - appData.displayCount) / dataDisplayRatio;
+        signatureCountElement.textContent = parseInt(appData.displayCount) || 'loading..';
+    }
 
-        // this should probably be in the data update?
-        signatureRate.style['font-size'] = Math.min(((result.rate + 1) * 50), 300) + 'px';
-        var color = 'hsl(' + (50-(result.rate * 4)) + ',' + (result.rate * 100) + '%,' + result.rate * 100 + '%)';
-        var shadowColor = 'hsl(' + (50-(result.rate * 4)) + ',' + (result.rate * 100) + '%,50%)';
-        var shadow = '0 0 ' + result.rate * 100 + 'px ' + shadowColor;
-        signatureRate.style['color'] = color;
-        signatureRate.style['text-shadow'] = shadow + ', ' + shadow;
-    }, displayInterval);
+    function initialiseTotal(){
+        setInterval(function(){
+            updateTotal();
+            updateRate();
+        }, displayInterval);
+    }
 
-    var rateChartWrapper = document.getElementById('ratechart');
-
-    rateChart = new Highcharts.Chart({
-        chart: {
-            renderTo: rateChartWrapper,
-            defaultSeriesType: 'spline',
-            backgroundColor: 'transparent'
-        },
-        colors:[
-            'white'
-        ],
-        title: {
-            text: 'Signatures per second over time',
-            style:{
-                color: 'white'
-            }
-        },
-        tooltip:{
-            backgroundColor: 'black',
-            color: 'white'
-        },
-        xAxis: {
-            categories: ['seconds'],
-            tickInterval: 10,
-            gridLineWidth:0,
-            labels:{
-                enabled: false
-            }
-        },
-        yAxis: {
+    function initialiseRateChart(){
+        var rateChartWrapper = document.getElementById('ratechart');
+        rateChart = new Highcharts.Chart({
+            chart: {
+                renderTo: rateChartWrapper,
+                defaultSeriesType: 'spline',
+                backgroundColor: 'transparent'
+            },
+            colors:[
+                'white'
+            ],
             title: {
-                text: 'Signatures'
-            },
-            labels:{
+                text: 'Signatures per second over time',
                 style:{
                     color: 'white'
                 }
             },
-            title:{
-                style:{
-                    color: 'white'
+            tooltip:{
+                backgroundColor: 'black',
+                color: 'white'
+            },
+            xAxis: {
+                categories: ['seconds'],
+                tickInterval: 10,
+                gridLineWidth:0,
+                labels:{
+                    enabled: false
                 }
             },
-            gridLineWidth:0
-        },
-        series:[{
-            name: 'Signature Rate',
-            data: []
-        }]
-    });
-};
+            yAxis: {
+                title: {
+                    text: 'Signatures'
+                },
+                labels:{
+                    style:{
+                        color: 'white'
+                    }
+                },
+                title:{
+                    style:{
+                        color: 'white'
+                    }
+                },
+                gridLineWidth:0
+            },
+            series:[{
+                name: 'Signature Rate',
+                data: []
+            }]
+        });
+        setInterval(updateRateGraph, displayInterval * 50);
+    }
+
+    function initialiseDataPoll(){
+        getData();
+        setInterval(getData, dataInterval);
+    }
+
+    function initialseDOMStructure(){
+        document.getElementsByClassName('current')[0].appendChild(results);
+    }
+
+    window.onload = function(){
+        initialseDOMStructure();
+        initialiseDataPoll();
+        initialiseTotal();
+        initialiseRateChart();
+    };
+}());
